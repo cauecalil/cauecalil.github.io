3a47f894fdf4fe09625edd251952b68bbbe406a5
Cauê <45627677+CaueDev@users.noreply.github.com>
2022-03-07 05:23:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CaueDev.github.io
---
+++ js/buttons.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("button").hide();
+    $("button").each(function (i) {
+        var name = $(this).attr('value');
+        $(this).delay(i * 100).slideDown(60);
+        $(this).html(name);
+    });
+});
